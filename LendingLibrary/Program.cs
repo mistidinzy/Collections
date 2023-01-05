f659514aa713b05b3393904c350fc8bad4a13534
Misti <mistidinzy@gmail.com>
2023-01-04 19:49:13 -0600

    starts user navigation menu

--- LendingLibrary/Program.cs
+++ LendingLibrary/Program.cs
@@ -2,11 +2,27 @@
 
 namespace LendingLibrary
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello, Welcome to the Library!");
+
+      userMenu();
+     
+
+
+
     }
+
+    static void userMenu()
+    {
+      Console.WriteLine("Please choose one of these options.");
+      Console.WriteLine("1. View Catalog");
+      Console.WriteLine("2. Add a Book");
+      Console.WriteLine("3. Borrow a Book");
+      Console.WriteLine("4. Delete a Book");
+    }
+
+  }
 }
