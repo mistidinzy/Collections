0f73e218704e166b473e59e67b7a01c210610a2d
Misti <mistidinzy@gmail.com>
2023-01-04 22:03:34 -0600

    Starts Backpack class with IBag interface

---
+++ LendingLibrary/Backpack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace LendingLibrary
+{
+  public class Backpack<T> : IBag<T>
+  {
+    public Backpack()
+    {
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Pack(T item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public T Unpack(int index)
+    {
+      throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
+
